49a3f8053ace3f26a155ff48c0225c902ab23660
Antonio Fonseca <antoniovicgf@gmail.com>
2026-06-24 23:06:13 -0300

    formatation fixed

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -858,9 +858,9 @@ def plot_global_change_frequency_bar_chart(
         inplace=True,
     )
 
-        # Remove stable pixels (0 changes) from both the stacked bars and legend
-        if "0" in df.columns:
-            df = df.drop(columns=["0"])
+    # Remove stable pixels (0 changes) from both the stacked bars and legend
+    if "0" in df.columns:
+        df = df.drop(columns=["0"])
 
     # 5. Sort columns numerically
     sorted_cols = sorted(
